2da7127d378fcd530f8682192cd1939671106556
Claudimar Bezerra <claudimards@gmail.com>
2021-01-06 16:21:03 -0300

    Registrando service worker

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// registrar serviceWorker caso o navegador aceite
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('/sw.js')
+        .then((reg) => console.log('service worker registered', reg))
+        .catch((err) => console.log('service worker not registered', err))
+}
